41af01e29f7b2142126df46a990e9ffb5ea42f0a
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-07-19 01:01:15 +0300

    Add files via upload

---
+++ ECUAL/LCD/LCD_Priv.h
@@ -0,0 +1,21 @@
+﻿/*
+ * LCD_Priv.h
+ *
+ * Created: 14/02/2023 08:45:16 ص
+ *  Author: Menna Eid
+ */ 
+
+
+#ifndef LCD_PRIV_H_
+#define LCD_PRIV_H_
+
+#define EIGHT  8
+#define FOUR   4
+static inline void LCD_invidSendCommend(uint8_t _command);
+static void LCD_Latch(uint8_t _u8command);
+
+
+
+
+
+#endif /* LCD_PRIV_H_ */
